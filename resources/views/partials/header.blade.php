e5cf541c75c36873b3dba0786bb64d321c6cb8b6
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-03-06 15:28:33 +0700

    refactor styling header untuk tata letak yang lebih baik

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,5 @@
 <div class="fixed w-full top-0 z-50 bg-[#faf9f9]">
-  <div class="xl:hidden block mt-5 rounded-lg py-1 bg-[#1EA05E] text-white mx-4">
+  <div class="xl:hidden block py-1 bg-[#1EA05E] text-white">
     <h3 class="md:text-5xl text-3xl font-bold xl:text-start text-center">SERUIT</h3><br>
     <p class="-mt-5 md:text-lg text-sm xl:text-start text-center">
       Satu Ruang Informasi untuk Inovasi Terintegrasi
