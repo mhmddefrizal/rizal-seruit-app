cac776e7dc8daee84fbc84539e6a8d308241581e
mhmddefrizal <muhammaddefrizal08@gmail.com>
2026-03-18 21:04:26 +0700

    feat: Initialize application head with Tailwind CSS and carousel styling.

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,6 +1,11 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>{{ config('app.name', 'SERUIT-BPS') }}{{ isset($title) ? ' | ' . $title : '' }}</title>
+{{-- Google Fonts: Plus Jakarta Sans --}}
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+
 {{-- Load CSS & JS utama (hasil npm run build) --}}
 @vite(['resources/css/app.css', 'resources/js/app.js'])
 
