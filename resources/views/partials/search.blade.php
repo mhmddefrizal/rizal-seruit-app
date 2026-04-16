3bb673a23ee5329ff7252651c2a2179bd7ad7301
Muhammad Defrizal <muhammaddefrizal08@gmail.com>
2026-04-16 13:29:10 +0700

    fix: enhance spacing and max-width utilities for improved layout consistency

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -5,7 +5,7 @@
         box-shadow: 0 0 0 1px #a51c31;
     }
 </style>
-<div class="relative my-3 w-full max-w-6xl lg:max-w-xl mx-auto"
+<div class="relative my-6 w-full max-w-lg xl:max-w-6xl mx-auto"
     style="margin-top: calc(var(--header-height, 3.5rem) + 0.25rem);">
     <input id="search_1" type="text" placeholder="Mau cari aplikasi apa ya..."
         class="search_app w-full px-4 py-2 border border-neutral-200 rounded-lg">
