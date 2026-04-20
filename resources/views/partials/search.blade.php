ae939dbd0b44eb4b125288288425dde6d8ec7c9f
Muhammad Defrizal <muhammaddefrizal08@gmail.com>
2026-04-20 08:46:23 +0700

    fix: add margin utilities for improved layout consistency in components

--- resources/views/partials/search.blade.php
+++ resources/views/partials/search.blade.php
@@ -5,8 +5,7 @@
         box-shadow: 0 0 0 1px #a51c31;
     }
 </style>
-<div class="relative my-6 w-full max-w-lg xl:max-w-6xl mx-auto"
-    style="margin-top: calc(var(--header-height, 3.5rem) + 0.25rem);">
+<div class="relative my-6 mt-18 xl:mt-20 mt-2xl-22 w-full max-w-lg xl:max-w-6xl mx-auto">
     <input id="search_1" type="text" placeholder="Mau cari aplikasi apa ya..."
         class="search_app w-full px-4 py-2 border border-neutral-200 rounded-lg">
     <img src="/img/search.svg" alt="Search" class="absolute top-1/2 right-6 transform -translate-y-1/2 w-5 h-5">
